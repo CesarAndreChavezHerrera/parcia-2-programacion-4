b935889c0ef120cff0a46e4ba546b31fb6e6e5c1
Angel <villatoroperez2000@gmail.com>
2022-04-21 21:19:56 -0600

    Cosa para bootstrap

--- aliementosapiconsumo/src/components/edit.vue
+++ aliementosapiconsumo/src/components/edit.vue
@@ -1,4 +1,7 @@
 <template>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>  
     <div>
         <h1>Modificacion de los datos ID:{{id_}}</h1>
         
